1dcea0e7565d1c71b189809ade9ea58247023b6d
odilonHNV <odilonhouenouvo@gmail.com>
2025-04-05 15:22:54 +0100

    Amélioration du Js

--- Blog/blogApp/static/blogApp/js/base.js
+++ Blog/blogApp/static/blogApp/js/base.js
@@ -2,4 +2,9 @@ const profilImage = document.querySelector(".profil_image")
 const dropDown = document.querySelector(".drop_down")
 profilImage.addEventListener('click',()=>{dropDown.classList.toggle('disp')})
         
-
+const body = document.querySelector("body")
+body.addEventListener("click",
+    ()=>{
+        message_info.style.display="none"
+    }
+)
--- Blog/blogApp/static/blogApp/js/list_article.js
+++ Blog/blogApp/static/blogApp/js/list_article.js
@@ -1,8 +1,1 @@
-const body = document.querySelector("body")
 const message_info = document.querySelector(".message")
-
-body.addEventListener("click",
-    ()=>{
-        message_info.style.display="none"
-    }
-)
